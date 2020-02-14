a04a2c426f968b6666735c71e62c6be2517de340
ltt <823030735@qq.com>
2020-02-15 00:53:37 +0800

    修改前端某些验证

---
+++ js/judge.js
@@ -0,0 +1,18 @@
+! function () {
+    $.ajaxSetup( {
+        //设置ajax请求结束后的执行动作
+        complete : function(XMLHttpRequest, textStatus) {
+            // 通过XMLHttpRequest取得响应头，REDIRECT
+            var redirect = XMLHttpRequest.getResponseHeader("REDIRECT");//若HEADER中含有REDIRECT说明后端想重定向
+            if (redirect == "REDIRECT") {
+                var win = window;
+                while (win != win.top){
+                    win = win.top;
+                }
+                //将后端重定向的地址取出来,使用win.location.href去实现重定向的要求
+                alter("未登录");
+                win.location.href= XMLHttpRequest.getResponseHeader("CONTEXTPATH");
+            }
+        }
+    });
+}();
