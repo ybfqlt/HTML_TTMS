2c822fecd32f13979a939d6dec9f624824f714f8
ltt <823030735@qq.com>
2020-02-15 15:35:22 +0800

    逻辑修改

--- js/judge.js
+++ js/judge.js
@@ -5,13 +5,13 @@
             // 通过XMLHttpRequest取得响应头，REDIRECT
             var redirect = XMLHttpRequest.getResponseHeader("REDIRECT");//若HEADER中含有REDIRECT说明后端想重定向
             if (redirect == "REDIRECT") {
-                var win = window;
-                while (win != win.top){
-                    win = win.top;
-                }
+                // var win = window;
+                // while (win != win.top){
+                //     win = win.top;
+                // }
+                alert("未登录，请先登录");
                 //将后端重定向的地址取出来,使用win.location.href去实现重定向的要求
-                alter("未登录");
-                win.location.href= XMLHttpRequest.getResponseHeader("CONTEXTPATH");
+               window.top.location.href= XMLHttpRequest.getResponseHeader("PATH");
             }
         }
     });
